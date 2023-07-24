1f9b9b59bcbd332bac47fc3e7d78576fca0c2ac0
Shanti <fishfinna12@gmail.com>
2023-07-23 19:11:01 -0700

    added the typewriter

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,34 +1,63 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { Stars } from "../../components/stars/Stars";
 import "./Home.scss";
 
 export default function Home() {
-  const typewriterRef = useRef<HTMLParagraphElement | null>(null);
-
-  // const typewriterContent = [
-  //   "web design.",
-  //   "graphic design.",
-  //   "backend programming.",
-  //   "database management.",
-  //   "fun stuff!",
-  // ];
+  const typewriterRef = useRef(null);
+  const txtRef = useRef("");
+  const isDeletingRef = useRef(false);
+  const loopNumRef = useRef(0);
+  const [typewriterState, updateTypeWriter] = useState<string>("");
+  const typewriterContent = [
+    "web development.",
+    "graphic design.",
+    "programming.",
+    "fun stuff!",
+  ];
 
   useEffect(() => {
-    //finish this to change the text
-    null;
+    const tick = () => {
+      const i = loopNumRef.current % typewriterContent.length;
+      const fullTxt = typewriterContent[i];
+
+      if (isDeletingRef.current) {
+        txtRef.current = fullTxt.substring(0, txtRef.current.length - 1);
+      } else {
+        txtRef.current = fullTxt.substring(0, txtRef.current.length + 1);
+      }
+
+      updateTypeWriter(txtRef.current);
+
+      let delta = 140 - Math.random() * 100;
+
+      if (isDeletingRef.current) {
+        delta /= 2;
+      }
+
+      if (!isDeletingRef.current && txtRef.current === fullTxt) {
+        delta = 500;
+        isDeletingRef.current = true;
+      } else if (isDeletingRef.current && txtRef.current === "") {
+        isDeletingRef.current = false;
+        loopNumRef.current++;
+        delta = 1000;
+      }
+
+      setTimeout(tick, delta);
+    };
+
+    tick();
   }, []);
 
   return (
     <>
       <Stars />
       <div className="hero">
-        <div className="hero-content">
-          <h1>Shanti S.</h1>
+        <h1>Shanti S.</h1>
 
-          <p className="hero-typewriter" ref={typewriterRef}>
-            I do
-          </p>
-        </div>
+        <p className="hero-typewriter" ref={typewriterRef}>
+          I do {typewriterState}
+        </p>
       </div>
     </>
   );
