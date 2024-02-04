68717feb1cd1eff19e78f664d4540bf96bbf6090
Shanti <fishfinna12@gmail.com>
2024-02-03 21:55:53 -0800

    email

--- src/components/email/email.tsx
+++ src/components/email/email.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useFormik } from "formik";
 import "./email.scss";
 
@@ -8,48 +7,66 @@ export function Email() {
       email: "",
       comments: "",
     },
-    onSubmit: () => {
-      null;
+    onSubmit: (formData) => {
+      fetch("https://formsubmit.co/fishfinna12@gmail.com", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
     },
   });
 
   return (
-    <div className="email">
-      <form
-        className="about-email"
-        action="https://formsubmit.co/fishfinna12@gmail.com"
-      >
-        <input type="text" name="_honey" style={{ display: "none" }} />
+    <>
+      <div className="email">
+        <h1>Send me an Email</h1>
+        <p>Ask me anything or just say hi!</p>
+        <form
+          className="about-email"
+          action="https://formsubmit.co/fishfinna12@gmail.com"
+          method="POST"
+        >
+          <input type="text" name="_honey" style={{ display: "none" }} />
 
-        <input type="hidden" name="_captcha" value="false" />
-        <input type="hidden" name="_invalid" value="false" />
-        <input
-          type="hidden"
-          name="_next"
-          value="https://fishfinna.github.io/personal-website/"
-        />
-        <div className="email-content">
-          <label htmlFor="email">Return Email:</label>
+          <input type="hidden" name="_captcha" value="false" />
+          <input type="hidden" name="_invalid" value="false" />
           <input
-            type="email"
-            id="return"
-            name="email"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
+            type="hidden"
+            name="_next"
+            value="https://fishfinna.github.io/personal-website/"
           />
-          <label htmlFor="comments">Message:</label>
-          <textarea
-            name="comments"
-            id="comments"
-            rows="5"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.comments}
-          />
-          <button type="submit">Send It Off</button>
-        </div>
-      </form>
-    </div>
+          <div className="email-content">
+            <label htmlFor="email">Return Email:</label>
+            <input
+              type="email"
+              id="return"
+              name="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+            <label htmlFor="comments">Message:</label>
+            <textarea
+              name="comments"
+              id="comments"
+              rows={5}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.comments}
+            />
+            <button type="submit">Send It Off</button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
