d885bcdb6c834fd6a82caa0525ab9bec16fd8d8b
Shanti <fishfinna12@gmail.com>
2024-02-08 19:44:25 -0800

    error page

--- src/pages/error-page/ErrorPage.tsx
+++ src/pages/error-page/ErrorPage.tsx
@@ -1,14 +1,15 @@
 import { Stars } from "../../components/stars/Stars";
 import { Link } from "react-router-dom";
+import "./ErrorPage.scss";
 
 export default function ErrorPage() {
   return (
-    <>
+    <div className="error-page">
       <Stars />
       <h1>404 Error: Page not found</h1>
       <Link to="/">
         <button>Return Home</button>
       </Link>
-    </>
+    </div>
   );
 }
