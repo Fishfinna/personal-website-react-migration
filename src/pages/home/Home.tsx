bfb7e78a87d32a132df453bfe4659c113f3b6395
Shanti <fishfinna12@gmail.com>
2024-03-17 17:30:05 -0700

    fix for images

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -12,10 +12,7 @@ export default function Home() {
     <>
       <Waves />
       <Stars />
-      <img
-        src="/personal-website-react-migration/images/treeline.png"
-        className="treeline"
-      ></img>
+      <img src="/images/treeline.png" className="treeline"></img>
       <div className="hero">
         <div className="hero-text">
           <h1>Shanti S.</h1>
