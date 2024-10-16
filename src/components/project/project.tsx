4c4da961d8d6d3fa48496504f2894395bc5d91c5
shanti s <fishfinna12@gmail.com>
2024-10-16 12:03:55 -0700

    updated the project

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -3,9 +3,13 @@ interface ProjectParams {
     name: string,
     url: string,
     thumbnail: string
+    children?: React.ReactNode;
 }
 
 
 export function Project(params: ProjectParams) {
-    return <></>;
+    return <div>
+        <h2>{params.name}</h2>
+        <div>{params.children}</div>
+    </div>;
 }
