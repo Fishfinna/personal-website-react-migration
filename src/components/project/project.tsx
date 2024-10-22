326d4c41fb609899c6d73e4d330f8fb199282bab
Shanti <fishfinna12@gmail.com>
2024-10-21 19:40:43 -0700

    styles and better layout for the project component

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -1,17 +1,24 @@
 interface ProjectParams {
   name: string;
-  url: string;
   thumbnail: string;
+  websiteUrl?: string;
+  codeUrl?: string;
   children?: React.ReactNode;
 }
 
 export function Project(params: ProjectParams) {
+  const projectImage = {
+    "--project-thumbnail-url": `url(${params.thumbnail})`,
+  } as React.CSSProperties;
+
   return (
-    <div>
+    <div className="project-container" style={projectImage}>
       <h2>{params.name}</h2>
-      <a href={params.url}>view</a>
-      <img src={params.thumbnail} />
       <div>{params.children}</div>
+      <div className="project-buttons">
+        {params.codeUrl ? <a href={params.codeUrl}>code</a> : null}
+        {params.websiteUrl ? <a href={params.websiteUrl}>website</a> : null}
+      </div>
     </div>
   );
 }
