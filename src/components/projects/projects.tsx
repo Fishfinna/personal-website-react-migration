72417919bbd060ec363b3338a771ffd68dd94481
Shanti <fishfinna12@gmail.com>
2024-11-07 18:56:06 -0800

    updates to responsiveness

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -17,6 +17,15 @@ export function Projects() {
       websiteUrl: "https://example.com",
       codeUrl: "http://example.com",
     },
+    {
+      name: "example",
+      children:
+        "Example  text more goes here I did stuff and I did tghings and now the things are stufExample  text more goes here I did stuff and I did tghings and now the things are stExample  text more goes here I did stuff and I did tghings and now the things are stuff and uff and f and it is all good.",
+      thumbnail:
+        "https://images.pexels.com/photos/28795942/pexels-photo-28795942/free-photo-of-elegant-bridal-bouquet-with-pink-roses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      websiteUrl: "https://example.com",
+      codeUrl: "http://example.com",
+    },
   ];
 
   projects.forEach((project, index) => {
@@ -36,5 +45,10 @@ export function Projects() {
     );
   });
 
-  return <div className="project-list">{projectList}</div>;
+  return (
+    <>
+      <h1>Projects</h1>
+      <div className="project-list">{projectList}</div>
+    </>
+  );
 }
